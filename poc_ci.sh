66767e1819aebfddda784dd9705d5be73fd36db7
Christian Tragesser <christian.tragesser@asynchrony.com>
2018-07-05 10:45:55 -0500

    implement uat tests and docker image CVE scanning

---
+++ poc_ci.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT=`basename "$0"`
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ ! $# == 1 ]; then
+    printf "\n $SCRIPT script needs a parameter: unit, test, etc. \n ex: '$SCRIPT test' \n\n"
+    exit 1
+fi
+
+OPTION=$1
+NETWORK="ci_net"
+CYAN='\033[1;36m'
+PURPLE='\033[1;35m'
+NC='\033[0m'
+
+function ci {
+  case $OPTION in
+    test) build
+          PULL_IMAGES=(christiantragesser/dind-ci)
+          pull_images
+          uat
+    ;;
+    scan) PULL_IMAGES=(arminc/clair-db:latest christiantragesser/dind-ci arminc/clair-local-scan:v2.0.1 christiantragesser/clair-scanner)
+          pull_images
+          image_scan
+    ;;
+    purge) purge_containers
+    ;;
+    *) printf "\n *** Selection $OPTION not found, exiting. ***\n\n"
+          exit 1
+  esac
+}
+
+function build {
+  echo -e "${CYAN}---- Building docker image ----${NC}"
+
+  docker build -t local/poc_app .
+  
+  echo -e "${PURPLE}---- Build complete ----${NC}"
+}
+
+function uat {
+  echo -e "${CYAN}---- Start UAT tests ----${NC}"
+
+  docker network create $NETWORK || true
+  docker run -d --net $NETWORK --name poc-app local/poc_app
+  docker run --rm -i \
+    -v $PWD:/tmp \
+    -w /tmp/tests \
+    --net $NETWORK \
+    christiantragesser/dind-ci bash -C uat.sh poc-app
+  docker tag local/poc_app christiantragesser/poc_app
+
+  echo -e "${PURPLE}---- Testing complete ----${NC}"
+}
+
+function image_scan {
+  echo -e "${CYAN}---- Docker image CVE scan ----${NC}"
+ 
+  docker network create $NETWORK || true
+  docker run -d --name postgres --net $NETWORK arminc/clair-db:latest
+  docker run --rm --net $NETWORK christiantragesser/dind-ci bash -c "while ! timeout 1 bash -c 'cat < /dev/null > /dev/tcp/postgres/5432' &>/dev/null; do :; done"
+  docker run -d --name clair --net $NETWORK arminc/clair-local-scan:v2.0.1
+  docker run --rm -i --net $NETWORK \
+    -v $PWD:/tmp \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    christiantragesser/clair-scanner sh -c "/opt/clair-scan.sh christiantragesser/poc_app"
+
+  echo -e "${PURPLE}---- CVE scan complete ----${NC}"
+}
+
+function pull_images {
+  for IMAGE in ${PULL_IMAGES[@]};
+  do
+    echo -e "${CYAN}+ Pulling image ${IMAGE}:${NC}"
+    docker pull $IMAGE;
+  done  
+}
+
+function purge_containers {
+  echo -e "${CYAN}---- CI docker container clean up ----${NC}"
+
+  docker rm -f poc-app postgres clair || true
+  docker network rm $NETWORK || true
+
+  echo -e "${PURPLE}---- clean up complete ----${NC}"
+}
+
+ci
--- tests/uat.sh
+++ tests/uat.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 
-echo -e "\n****** starting automated tests ********\n"
+echo -e "\nImplementing tests:"
 
 curl -s $1 | grep --quiet "Server hostname"
 hostResult=$?
 
 if [ $hostResult -eq 0 ]; then
-  echo "+ hostname content seen"
+  echo -e "\t+ hostname content seen"
 else
-  echo "- hostname content not seen" && exit $hostResult
+  echo -e "\t- hostname content not seen" && exit $hostResult
 fi
 
 curl -s $1 | grep --quiet "Server LAN IP address"
 ipResult=$?
 
 if [ $ipResult -eq 0 ]; then
-  echo "+ LAN IP content seen"
+  echo -e "\t+ LAN IP content seen"
 else
-  echo "- LAN IP content not seen" && exit $ipResult
+  echo -e "\t- LAN IP content not seen" && exit $ipResult
 fi
 
 curl -s $1 | grep --quiet "background-color:white"
 backgroundResult=$?
 
 if [ $backgroundResult -eq 0 ]; then
-  echo "+ background color check passed"
+  echo -e "\t+ background color check passed"
 else
-  echo "- background color check failed" && exit $backgroundResult
+  echo -e "\t- background color check failed" && exit $backgroundResult
 fi
-
-echo -e "\n****** testing complete ********\n"
