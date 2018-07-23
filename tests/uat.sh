526019dc50da8d21ea5f82140ec1166619d441f8
Christian Tragesser <christian.tragesser@asynchrony.com>
2018-07-23 07:59:29 -0500

    add unicode status to tests

--- tests/uat.sh
+++ tests/uat.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
+CHECKMARK="\xE2\x9C\x94"
+CROSSBONES="\xE2\x98\xA0"
 echo -e "\nImplementing tests:"
 
 curl -s $1 | grep --quiet "Server hostname"
 hostResult=$?
 
 if [ $hostResult -eq 0 ]; then
-  echo -e "\t+ hostname content seen"
+  echo -e "\t$CHECKMARK  Hostname content seen"
 else
-  echo -e "\t- hostname content not seen" && exit $hostResult
+  echo -e "\t$CROSSBONES  Hostname content not seen" && exit $hostResult
 fi
 
 curl -s $1 | grep --quiet "Server LAN IP address"
 ipResult=$?
 
 if [ $ipResult -eq 0 ]; then
-  echo -e "\t+ LAN IP content seen"
+  echo -e "\t$CHECKMARK  LAN IP content seen"
 else
-  echo -e "\t- LAN IP content not seen" && exit $ipResult
+  echo -e "\t$CROSSBONES  LAN IP content not seen" && exit $ipResult
 fi
 
 curl -s $1 | grep --quiet "background-color:white"
 backgroundResult=$?
 
 if [ $backgroundResult -eq 0 ]; then
-  echo -e "\t+ background color check passed"
+  echo -e "\t$CHECKMARK  Background color check passed"
 else
-  echo -e "\t- background color check failed" && exit $backgroundResult
+  echo -e "\t$CROSSBONES  Background color check failed" && exit $backgroundResult
 fi
