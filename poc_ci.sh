b68ec807c6d96d8e67f3df4ed043218505795af0
Christian Tragesser <christian.tragesser@asynchrony.com>
2018-08-04 08:43:39 -0500

    use alpine borne shell + nc to check for db connectivity rather than ubuntu bash + tcp socket check

--- poc_ci.sh
+++ poc_ci.sh
@@ -54,13 +54,13 @@ function uat {
 }
 
 function image_scan {
-  PULL_IMAGES=(arminc/clair-db:latest ubuntu arminc/clair-local-scan:v2.0.3 christiantragesser/clair-scanner)
+  PULL_IMAGES=(arminc/clair-db:latest arminc/clair-local-scan:v2.0.3 christiantragesser/clair-scanner)
   pull_images
   echo -e "${CYAN}---- Docker image CVE scan ----${NC}"
  
   docker network create $NETWORK || true
   docker run -d --name postgres --net $NETWORK arminc/clair-db:latest
-  docker run --rm --net $NETWORK ubuntu bash -c "while ! timeout 1 bash -c 'cat < /dev/null > /dev/tcp/postgres/5432' &>/dev/null; do :; done"
+  docker run --rm --net $NETWORK christiantragesser/clair-scanner /bin/sh -c "while ! timeout -t 1 sh -c 'nc -zv postgres 5432' &>/dev/null; do :; done"
   docker run -d --name clair --net $NETWORK arminc/clair-local-scan:v2.0.3
   docker run --rm -i --net $NETWORK \
     -v $PWD:/tmp \
